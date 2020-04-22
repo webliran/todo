cd1aadb3aab586b6dd23906bde0875ae9ee98e06
webliran <liranhecht@gmail.com>
2020-04-22 19:24:26 +0300

    third commit

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const express =  require('express');
+const express = require('express');
 const app = express();
 const cors = require('cors');
 const path = require('path');
@@ -9,12 +9,12 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded())
 
-app.use("/todo",todoRouter);
+app.use("/todo", todoRouter);
 
 app.use(express.static(path.join(__dirname, 'build')));
 
-app.get("/",function(){
-
-})
+app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 app.listen(5000);
